b401a76d338968fa19ad4a114f4092ecfbda9ddf
Kirill Maslinsky <kirill@altlinux.org>
2016-04-18 12:27:15 +0300

    remote: s/localhost/127.0.0.1 in apache port config

--- remote/setup-corpus-environment.sh
+++ remote/setup-corpus-environment.sh
@@ -7,4 +7,5 @@ a2disport http
 cd /etc/httpd2/conf/
 cp ports-available/http-localhost-8088.conf ports-available/"$port".conf
 sed -i "s/8088/$port/g" ports-available/"$port".conf
+sed -i "/^Listen/s/localhost/127.0.0.1/" ports-available/"$port".conf
 a2enport "$port"
