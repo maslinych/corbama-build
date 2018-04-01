c2d64d1ba551a6b5f6d3838c9e50bc20ac9c80db
Kirill Maslinsky <kirill@altlinux.org>
2018-04-01 18:14:49 +0200

    fix remote scripts for re-creating testing env

--- remote/install-all-corpora.sh
+++ remote/install-all-corpora.sh
@@ -5,12 +5,12 @@ corpora=(built/*.tar.xz)
 
 for corptar in "${corpora[@]}"
 do
-    ln "$corptar" "$environment"/chroot/.in/
+    ln -v "$corptar" "$environment"/chroot/.in/
     corpnames="$(cat "${corptar%%.tar.xz}".setup.txt | cut -d' ' -f2-)"
     for corpus in $corpnames
     do
-        hsh-run --rooter "$environment" "rm -rf /var/lib/manatee/{data,registry,vert}/$corpus"
+        hsh-run --rooter "$environment" -- rm -rf /var/lib/manatee/{data,registry,vert}/$corpus
     done
-    hsh-run --rooter "$environment" "tar --no-same-permissions --no-same-owner -xJvf ${corptar##built/} --directory /var/lib/manatee"
+    hsh-run --rooter "$environment" -- tar --no-same-permissions --no-same-owner -xJvf ${corptar##built/} --directory /var/lib/manatee
 done
 
--- remote/setup-all-corpora.sh
+++ remote/setup-all-corpora.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+environment="testing"
+cp -v bin/setup-bonito.sh "$environment"/chroot/.in/
 cat built/*.setup.txt | \
     awk '{for (i=2;i<=NF;i++) {corp[$1]=corp[$1]" "$i}}END{for (c in corp) {print c, corp[c]}}' | \
-    xargs setup-bonito.sh
+    xargs -t -L 1 hsh-run --root "$environment" -- sh setup-bonito.sh 
