209708b13812fbe321ecc27c73fa67acba5d35e9
Kirill Maslinsky <kirill@altlinux.org>
2018-04-01 09:09:50 +0200

    fix quoting in remote script

--- remote/setup-corpus.sh
+++ remote/setup-corpus.sh
@@ -4,4 +4,4 @@ site="$1"
 shift
 corplist="$@"
 cp bin/setup-bonito.sh "$environment"/chroot/.in/
-hsh-run --rooter "$environment" -- "setup-bonito.sh $corpname $corplist"
+hsh-run --rooter "$environment" -- sh -x setup-bonito.sh $site $corplist
