9830ef1091d3d0e36583a1c0e0c980c5e3d086af
Kirill Maslinsky <kirill@altlinux.org>
2018-04-02 17:15:37 +0200

    fix stop-env

--- remote/stop-env.sh
+++ remote/stop-env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 environment="$1"
 shift
-tmux has-session -t "$environment" || "$environment is not running" && exit 0
+tmux has-session -t "$environment" || exit 0
 tmux send-keys -t "$environment":0 \"service httpd2 stop\" Enter && 
 tmux kill-session -t "$environment"
