bbffadbcf575044ba27257483dae058e1ff80282
Kirill Maslinsky <kirill@altlinux.org>
2018-04-02 13:53:04 +0200

    remote start/stop scripts: check if session already exists

--- remote/stop-env.sh
+++ remote/stop-env.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 environment="$1"
 shift
-tmux send-keys -t "$environment":0 \"service httpd2 stop\" Enter
-tmux kill-session -t "$environment"
+tmux has-session -t "$environment" && \
+    tmux send-keys -t "$environment":0 \"service httpd2 stop\" Enter && \
+    tmux kill-session -t "$environment"
